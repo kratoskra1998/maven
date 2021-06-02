fa05610447be5a727f8dc84e25edca1da080a9f7
Rhenius <rheniusdevaraj98@gmail.com>
2021-06-02 11:54:34 +0530

    first

---
+++ first/src/main/java/com/syntel/demo/App.java
@@ -0,0 +1,13 @@
+package com.syntel.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
